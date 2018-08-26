ebd28ac5d63a78146ba99eeb186ef690d53cb1a9
Marian <Marian@192.168.0.159>
2018-08-26 16:06:18 +0300

    new commit 2

--- src/java9platonovmarian/Java9PlatonovMarian.java
+++ src/java9platonovmarian/Java9PlatonovMarian.java
@@ -18,7 +18,8 @@ public class Java9PlatonovMarian {
         // TODO code application logic here
         
         System.out.println("Hello my friend! ");
-        sum(1,2);
+        sum(2,2);
+        decrease(9,-1);
     }
     
     public static int sum (int a, int b){
@@ -26,8 +27,13 @@ public class Java9PlatonovMarian {
         sum=a+b;
         System.out.println("Sum of a and b = " +sum);
         return sum;
+    }
     
-        
+     public static int decrease (int a, int b){
+        int decrease;
+        decrease=a-b;
+        System.out.println("Decrease of a and b = " +decrease);
+        return decrease;
     }
     
 }
